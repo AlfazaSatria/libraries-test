4c4854fc273f03abbf60a294a33e829d89ecc2aa
Alfaza Satria <alfazasatria8@gmail.com>
2021-02-10 07:51:24 +0700

    Create card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,62 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-lg">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <h4 class="card-title text-center font-weight-bold">Libraries A</h4>
+                    <div class="row">
+                        <div class="col-md-4">
+                            Book Aa
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="col-md-4">
+                            Book B 
+                        </div>
+                        <div class="col-md-4">
+                            Book C 
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-lg">
+                <div class="card-body">
+                    <h4 class="card-title text-center font-weight-bold">Libraries A</h4>
+                    <div class="row">
+                        <div class="col-md-4">
+                            Book Aa
+                        </div>
+                        <div class="col-md-4">
+                            Book B 
+                        </div>
+                        <div class="col-md-4">
+                            Book C 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-lg">
+                <div class="card-body">
+                    <h4 class="card-title text-center font-weight-bold">Libraries A</h4>
+                    <div class="row">
+                        <div class="col-md-4">
+                            Book Aa
+                        </div>
+                        <div class="col-md-4">
+                            Book B 
+                        </div>
+                        <div class="col-md-4">
+                            Book C 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,10 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <title>Libraries Test</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -21,10 +20,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    Libraries Test
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -50,7 +49,7 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
